cfbfd8a9419c7238b95a81d2f11831c8e202bfcd
Fabiano Brancher <fabiano.brancher@hotmail.com>
2022-01-01 18:59:29 -0300

    Custom validation response

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscatalog.resources.exceptions;
 import java.io.Serializable;
 import java.time.Instant;
 
+import antlr.collections.List;
+
 public class StandardError implements Serializable {
 
 	private static final long serialVersionUID = 1L;
