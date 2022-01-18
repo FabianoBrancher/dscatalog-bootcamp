6b050595ac68f4d687ddd930f31032f7bd94700c
Fabiano Brancher <fabiano.brancher@hotmail.com>
2022-01-18 15:40:02 -0300

    Implementing OAuth2 Authorization Server

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
@@ -3,8 +3,6 @@ package com.devsuperior.dscatalog.resources.exceptions;
 import java.io.Serializable;
 import java.time.Instant;
 
-import antlr.collections.List;
-
 public class StandardError implements Serializable {
 
 	private static final long serialVersionUID = 1L;
